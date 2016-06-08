63cdc17dd8be45f7ec6f92dea3d28b673da3e9af
rakesh635 <rakesh635@gmail.com>
2016-06-08 16:18:14 +0530

    added

---
+++ app.js
@@ -0,0 +1,86 @@
+﻿var express = require('express');
+var path = require('path');
+var http = require('http');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var db = require('./helper/dbconnection.js'),
+	account = require('./routes/account.js'),	
+	dashboard = require('./routes/dashboard.js'),
+	env_route = require('./api/route/environment.js'),	
+	stack_route = require('./api/route/stack.js'),
+	service_route = require('./api/route/service.js'),
+	container_route = require('./api/route/container.js'),
+	repo_route = require('./api/route/repo.js'),
+	repo_webhook_route = require('./api/route/repo_webhook.js'),
+	volume_route = require('./api/route/volume.js'),	
+	host_route = require('./api/route/host.js'),
+	token_route = require('./api/route/token.js'),
+	ci_route = require('./api/route/ci.js'),
+	user_route=require('./api/route/portal/user.js'),
+	appconfig = require('./appconfig.js'),
+	errlogger = require('./logger/logger.js'),
+    customer = require('./routes/customer.js'),
+    application = require('./routes/application.js'),
+    user = require('./routes/user.js'),
+    environment = require('./routes/environment.js'),
+	app = express(),
+	apitest = require('./apitest/apitest.js'),
+	api = require('./api/caas_express_socket_api.js');
+apitest.Start(false);
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(expressSession({ secret: 'Caas-IS-CP' }));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+app.use('/', account);
+
+app.use(function (req, res, next) {
+    if (req.session.accountdetails) {
+        res.locals.session = req.session;
+        next();
+    }
+    else {
+        //For API
+        next();
+    }
+});
+
+app.use('/dashboard', dashboard);
+app.use('/customer', customer);
+app.use('/application', application);
+app.use('/user', user);
+app.use('/environment', environment);
+var apipath = appconfig.Configurations.APIPath;
+
+app.use(apipath, env_route);
+app.use(apipath, service_route);
+app.use(apipath, stack_route);
+app.use(apipath, token_route);
+app.use(apipath, volume_route);
+app.use(apipath, host_route);
+app.use(apipath, repo_route);
+app.use(apipath, repo_webhook_route);
+app.use(apipath, ci_route);
+app.use(apipath, container_route);
+app.use(apipath, user_route);
+
+app.use(function (err, req, res, next) {
+	errlogger.Error('NA', 'NA', 'NA', 'NA', err.message, 1);
+	res.render('error', {errormessage:err.message});	
+});
+api.Init(app, appconfig.Configurations.API.IsSSLEnabled);
+module.exports = app;
---
+++ appconfig.js
@@ -0,0 +1,64 @@
+﻿"use strict";
+var appconfig = appconfig || {},
+	fs = require('fs');
+(function (config) {
+	config.Configurations = {
+		Url: 'http://192.168.54.207/v1/', //'http://192.168.56.102:8080/v1',
+		WSUrl: 'ws://192.168.54.207/',
+		Authorization: 'Basic ' + new Buffer('F1BA60FDFDB80504FF15' + ':' + 'wpLWzyahut2pWyJ9dCj3YikWfU987ch7A4K34tAg').toString('base64'),
+		Database: {
+			host: '192.168.54.206', user: 'root', password: 'password', database: 'CaaS_DEVDB_latest', multipleStatements: true
+		},
+		API: {
+			HOST: 'localhost',
+			PORT_HTTP: 8087,
+			PORT_HTTPS: 12345,
+			Domain:null,
+			IsSSLEnabled: false,
+			SSL_Certificate: {
+				/*pfx: fs.readFileSync('dockerservice.pfx'),
+				passphrase: '1234',*/
+				rejectUnauthorized: false,
+				key:fs.readFileSync('caas.key'),
+				cert:fs.readFileSync('ServerCertificate.crt'),
+				passphrase:'_J0HnNy_Gon3-M@D_'
+			}
+		},
+		APIPath: '/api/v1',
+		version: 'v1',
+		GitHubAPI: {
+			Url: 'https://api.github.com'
+		},
+		CIServerAPI: {
+			URL: 'http://192.168.54.205:8080',		
+			fullurl: 'http://admin:ab932bcf78150d0ee6211586b03ee380@192.168.54.205:8080',		
+			Authorization: 'Basic ' + new Buffer('admin' + ':' + 'ab932bcf78150d0ee6211586b03ee380').toString('base64'),
+		},
+		OpenStackAPI: {
+			TokenParamters: {
+				"auth": {
+					"tenantName": "admin",
+					"passwordCredentials": {
+						"username": "admin",
+						"password": "password"
+					}
+				}
+			},			
+			IdentityURL: 'http://192.168.54.102:35357/v2.0'
+		},
+		GitHubUrl: 'https://github.com/login/oauth/access_token?client_id=66237e95c6686b587886&client_secret=cb4fb3e37be51386390320062d637cfd68d8d83c&code=%s',		
+		EmailSettings: {
+			user: 'mahendran.m@prodapt.com',
+			password: 'Ammu@123',
+			host: 'exchange.prodapt.com',
+			domain: 'prodapt',
+			ssl: false,
+			port: 25,	
+            timeout: 15000,
+            url: 'http://192.168.54.215/'
+		},
+		ResetPasswordURL: 'http://localhost:8087/resetpassword?reset_id=',
+		MongoDB: 'mongodb://admin:password@192.168.54.171:27017/caasdaemondb' 
+	};
+}(appconfig));
+module.exports = appconfig;
